37dc8bfb923f1635f7fc0787484dcd9403145e47
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-21 13:35:23 -0800

    Add Instagram SVGs and put them into navigation

--- src/containers/Navigation.tsx
+++ src/containers/Navigation.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import style from './Navigation.module.css'
+import { Home, Explore, Direct, Like } from './../assets/icons/'
 
 function Navigation() {
   return (
-    <>
-      <nav className={style.navigation}>
-        Instagram
-        <input type="text" placeholder="search" />
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/direct-message">Dm</NavLink>
-        <NavLink to="/explore">Explore</NavLink>
-        <NavLink to="/likes">Likes</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-      </nav>
-    </>
+    <nav className={style.navigation}>
+      Instagram
+      <input type="text" placeholder="search" />
+      <NavLink to="/">
+        <Home />
+      </NavLink>
+      <NavLink to="/direct-message">
+        <Direct />
+      </NavLink>
+      <NavLink to="/explore">
+        <Explore />
+      </NavLink>
+      <NavLink to="/likes">
+        <Like />
+      </NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+    </nav>
   )
 }
 
