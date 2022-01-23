fe7a4e3dae22bec895acf55774c53f05c887e819
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-23 12:29:56 -0800

    Add conditional rendering of navigation icons

--- src/containers/Navigation.tsx
+++ src/containers/Navigation.tsx
@@ -1,24 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import style from './Navigation.module.css'
-import { Home, Explore, Direct, Like } from './../assets/icons/'
+import {
+  Home,
+  HomeFill,
+  Explore,
+  ExploreFill,
+  Direct,
+  DirectFill,
+  Like,
+  LikeFill
+} from './../assets/icons/'
+
+type Active = 'home' | 'dm' | 'explore' | 'likes' | 'profile'
 
 function Navigation() {
+  const [active, setActive] = useState<Active>('home')
+
+  const activate = (
+    isActive: boolean,
+    path: Active,
+    activeStyle?: React.CSSProperties,
+    nonActiveStyle?: React.CSSProperties
+  ): React.CSSProperties => {
+    if (isActive) {
+      setActive(path)
+      return activeStyle
+    }
+    return nonActiveStyle
+  }
+
   return (
     <nav className={style.navigation}>
-      <NavLink to="/">
-        <Home />
+      <NavLink
+        to="/"
+        style={(activeNav) => activate(activeNav.isActive, 'home')}
+      >
+        {active === 'home' ? <HomeFill /> : <Home />}
       </NavLink>
-      <NavLink to="/direct-message">
-        <Direct />
+      <NavLink
+        to="/direct-message"
+        style={(activeNav) => activate(activeNav.isActive, 'dm')}
+      >
+        {active === 'dm' ? <DirectFill /> : <Direct />}
       </NavLink>
-      <NavLink to="/explore">
-        <Explore />
+      <NavLink
+        to="/explore"
+        style={(activeNav) => activate(activeNav.isActive, 'explore')}
+      >
+        {active === 'explore' ? <ExploreFill /> : <Explore />}
       </NavLink>
-      <NavLink to="/likes">
-        <Like />
+      <NavLink
+        to="/likes"
+        style={(activeNav) => activate(activeNav.isActive, 'likes')}
+      >
+        {active === 'likes' ? <LikeFill /> : <Like />}
       </NavLink>
-      <NavLink to="/profile">Profile</NavLink>
+      <NavLink
+        to="/profile"
+        style={(activeNav) => activate(activeNav.isActive, 'profile')}
+      >
+        Profile
+      </NavLink>
     </nav>
   )
 }
