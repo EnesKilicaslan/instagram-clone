a03c69621528e299bc98d573525f36b5d30b15e3
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-25 08:03:50 -0800

    Add instagram logo and fix some irritating layout and color issues

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import style from './Header.module.css'
 import Navigation from './Navigation'
 import Container from './Container'
+import logo from '../assets/icons/logo.png'
 
 function Header() {
   return (
     <header className={style.header}>
       <Container className={style.headerContainer}>
         <>
-          <span className={style.logo}>Instagram</span>
+          <div className={style.logo}>
+            <img src={logo} />
+          </div>
 
           <span className={style.searchContainer}>
             <input type="text" placeholder="Search" className={style.search} />
