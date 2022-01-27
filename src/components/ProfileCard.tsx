aec787ad1cab8ac1c7591c51b758d149335bfabd
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-27 13:26:24 -0800

    Add suggestions part to the home page and create profile card component

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Avatar from './Avatar/Avatar'
+import style from './ProfileCard.module.css'
+
+function ProfileCard(props: {
+  type?: 'self' | 'other'
+  name: string
+  userName: string
+}) {
+  const { type = 'self', name, userName } = props
+
+  return (
+    <div className={style.container}>
+      <Avatar size={type === 'self' ? 'l' : 'm'} />
+      <div className={style.userInfo}>
+        <div className={style.slag}>{userName}</div>
+        <div className={style.content}>{name}</div>
+      </div>
+      <button className={style.profileType}>
+        {type === 'self' ? 'Switch' : 'Follow'}
+      </button>
+    </div>
+  )
+}
+
+export default ProfileCard
--- src/containers/Suggestions.tsx
+++ src/containers/Suggestions.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import ProfileCard from '../components/ProfileCard'
 import style from './Suggestions.module.css'
 
 function Suggestions() {
-  return <div className={style.suggestions}>Suggestions</div>
+  return (
+    <div className={style.container}>
+      <ProfileCard userName="enes.ki" name="Enes Kilicaslan" />
+
+      {/* view all */}
+      <div className={style.viewAll}>
+        <div className={style.forYouText}>Suggestions For You</div>
+        <button className={style.seeAllText}>See All</button>
+      </div>
+
+      <ProfileCard type="other" userName="bhscmidscs" name="followed by eric" />
+      <ProfileCard
+        type="other"
+        userName="oasdasdasd"
+        name="followed by nusret + 5 more"
+      />
+      <ProfileCard type="other" userName="metallica" name="New to Instagram" />
+      <ProfileCard type="other" userName="bhscmidscs" name="followed by eric" />
+      <ProfileCard
+        type="other"
+        userName="trainsing"
+        name="followed by bork + 12 more"
+      />
+    </div>
+  )
 }
 
 export default Suggestions
