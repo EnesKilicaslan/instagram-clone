740622d3474887311b595674f7b7076e4525ad49
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-29 14:45:40 -0800

    Add Profile Info section

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import profileImg from '../../assets/profile.jpg'
 import style from './Avatar.module.css'
 
-function Avatar(props: { size?: 's' | 'm' | 'l' }) {
+function Avatar(props: { size?: 's' | 'm' | 'l' | 'xl' }) {
   const { size = 'm' } = props
 
   return (
--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
+import { Settings } from '../../assets/icons'
+import Avatar from '../../components/Avatar/Avatar'
 import style from './Profile.module.css'
 
 function Profile() {
   return (
     <>
-      <div>Profile</div>
+      <div className={style.infoContainer}>
+        <div className={style.avatarContainer}>
+          <Avatar size="xl" />
+        </div>
+
+        <section className={style.info}>
+          <div className={style.edit}>
+            <span className={style.slug}> enes.ki</span>
+            <button className={style.editProfile}>Edit Profile</button>
+            <Settings />
+          </div>
+          <div className={style.numbers}>
+            <span>
+              <span className={style.numberItem}>15</span> posts
+            </span>
+            <span>
+              <span className={style.numberItem}>400</span> followers
+            </span>
+            <span>
+              <span className={style.numberItem}>300</span> following
+            </span>
+          </div>
+          <div className={style.name}>
+            <h1>Enes Kilicaslan</h1>
+            <h4>Slow Down</h4>
+          </div>
+        </section>
+      </div>
 
       <div className={style.profileNavigation}>
         <NavLink to="posts">Posts</NavLink>
