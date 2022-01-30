514d1d4f6935fa485765e29a080d4eaa050594ae
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-01-30 12:47:22 -0800

    Add profile navigation

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
-import { Settings } from '../../assets/icons'
+import {
+  Settings,
+  Posts as PostsIcon,
+  Tagged as TaggedIcon,
+  Saved as SavedIcon,
+  IGTV as IGTVIcon
+} from '../../assets/icons'
 import Avatar from '../../components/Avatar/Avatar'
 import style from './Profile.module.css'
 
+type RouteType = 'posts' | 'igtv' | 'saved' | 'tagged'
+
 function Profile() {
+  const [active, setActive] = useState<RouteType>('posts')
+
   return (
     <>
       <div className={style.infoContainer}>
@@ -36,11 +46,82 @@ function Profile() {
         </section>
       </div>
 
+      <div className={style.stories}></div>
+
       <div className={style.profileNavigation}>
-        <NavLink to="posts">Posts</NavLink>
-        <NavLink to="igtv">IGTv</NavLink>
-        <NavLink to="saved">Saved</NavLink>
-        <NavLink to="tagged">Tagged</NavLink>
+        <div className={style.navigationContainer}>
+          <div
+            className={
+              active === 'posts'
+                ? `${style.navigationItem} ${style.navigationItemActive}`
+                : style.navigationItem
+            }
+          >
+            <NavLink
+              to=""
+              style={({ isActive }) => {
+                isActive && setActive('posts')
+                return null
+              }}
+            >
+              <PostsIcon className={style.navigationIcon} />
+              POSTS
+            </NavLink>
+          </div>
+          <div
+            className={
+              active === 'igtv'
+                ? `${style.navigationItem} ${style.navigationItemActive}`
+                : style.navigationItem
+            }
+          >
+            <NavLink
+              to="igtv"
+              style={({ isActive }) => {
+                isActive && setActive('igtv')
+                return null
+              }}
+            >
+              <IGTVIcon className={style.navigationIcon} />
+              IGTV
+            </NavLink>
+          </div>
+          <div
+            className={
+              active === 'saved'
+                ? `${style.navigationItem} ${style.navigationItemActive}`
+                : style.navigationItem
+            }
+          >
+            <NavLink
+              to="saved"
+              style={({ isActive }) => {
+                isActive && setActive('saved')
+                return null
+              }}
+            >
+              <SavedIcon className={style.navigationIcon} />
+              SAVED
+            </NavLink>
+          </div>
+          <div
+            className={
+              active === 'tagged'
+                ? `${style.navigationItem} ${style.navigationItemActive}`
+                : style.navigationItem
+            }
+          >
+            <NavLink
+              to="tagged"
+              style={({ isActive }) => {
+                isActive && setActive('tagged')
+                return null
+              }}
+            >
+              <TaggedIcon className={style.navigationIcon} /> TAGGED
+            </NavLink>
+          </div>
+        </div>
       </div>
       <br />
       <Outlet />
@@ -49,3 +130,4 @@ function Profile() {
 }
 
 export default Profile
+//b38
